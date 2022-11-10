79df530dc037931a1930a82e68b7e5d08eb8d4ac
Santosh-0708 <santosh.rr07@gmail.com>
2022-11-10 16:12:57 +0530

    New File

---
+++ Naturalnos.js
@@ -0,0 +1,14 @@
+
+        function sum(num){
+            
+            if(num>0){
+                return sum+(num-1);
+            }else {
+                return num;
+            }
+            var res =  sum(5);
+            console.log(res);
+
+        }
+        
+   
